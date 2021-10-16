e98b22c0139015b6a2be0c8de5bf830230fcc503
TaintedEnergy <taintedenergy@gmail.com>
2021-10-16 23:52:30 +1300

    isPublicRole and isPolicingRole

--- lua/terrortown/entities/roles/revolutionary/shared.lua
+++ lua/terrortown/entities/roles/revolutionary/shared.lua
@@ -8,13 +8,18 @@ function ROLE:PreInitialize()
   self.color = Color(124, 78, 158, 255)
 
   self.abbr = "revol" -- abbreviation
-  self.surviveBonus = 0 -- bonus multiplier for every survive while another player was killed
-  self.scoreKillsMultiplier = 1 -- multiplier for kill of player of another team
-  self.scoreTeamKillsMultiplier = -4 -- multiplier for teamkill
-  self.unknownTeam = true -- player don't know their teammates
+  
+  self.defaultTeam = TEAM_INNOCENT
+	self.defaultEquipment = SPECIAL_EQUIPMENT
 
-  self.defaultTeam = TEAM_INNOCENT -- the team name: roles with same team name are working together
-  self.defaultEquipment = SPECIAL_EQUIPMENT -- here you can set up your own default equipment
+	self.score.killsMultiplier = 8
+	self.score.teamKillsMultiplier = -8
+	self.score.bodyFoundMuliplier = 3
+	self.fallbackTable = {}
+	self.unknownTeam = true
+
+	self.isPublicRole = true
+	self.isPolicingRole = true
 
   self.conVarData = {
     pct = 0.15, -- necessary: percentage of getting this role selected (per player)
@@ -23,8 +28,8 @@ function ROLE:PreInitialize()
     togglable = true, -- option to toggle a role for a client if possible (F1 menu)
 
     credits = 3,
-    creditsTraitorKill = 0,
-    creditsTraitorDead = 1,
+		creditsAwardDeadEnable = 1,
+		creditsAwardKillEnable = 0,
     random = 40,
 
     shopFallback = SHOP_FALLBACK_TRAITOR
